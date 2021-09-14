841dc008dfaffdc498e4b3881434518e6b02c842
Ragib <ragibhasan006@gmail.com>
2021-09-14 12:42:19 +0600

    yes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,23 +6,32 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+ 
+  public function boot(UrlGenerator $url){
+	  
+	  if(env('REDIRECT_HTTPS')){
+		  
+		  $url->formatSchema('https');
+		  
+	  }
+	  
+  }
+ 
+ 
+ public function register(){
+	  
+	  if(env('REDIRECT_HTTPS')){
+		  
+		  $this->app['request']->server->set('HTTPS',true);
+		  
+	  }
+	  
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 }
